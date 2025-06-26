86e38d97dd458ad9f8b2994c1c53c850e0244143
Kauri0000 <kauri.adeduro@hgs.hiddengeniusproject.org>
2025-06-26 13:59:42 -0700

    quickmath2 exit ticket

---
+++ phython/week1/week2/QuickMath2.md
@@ -0,0 +1,8 @@
+1. Define what a function is in your own words
+Something that repeats a code 
+2. What key word is used to define a function?
+def or define
+3. What does a return statement do?
+it returns data to you or the result
+4. What is the difference between arguments and parameters?
+Arguments are the real values you place in the function while parameters are the named placeholders in the function
---
+++ phython/week1/week2/quickmathpart2.py
@@ -0,0 +1,35 @@
+def Add(addend1, addend2):
+    return addend1 + addend2
+
+Add(5, 35)
+
+sum = Add(5, 35)
+
+print(sum)
+
+def Subtract(minuend, subtrahend):
+    return minuend - subtrahend
+
+Subtract(10, 5)
+
+sum = Subtract(10, 5)
+
+print(sum)
+
+def Multiply(multiplicand, multiplier):
+    return multiplicand * multiplier
+
+Multiply(5, 6)
+
+sum = Multiply(5, 6)
+
+print(sum)
+
+def Divide(dividend, divisor):
+    return dividend / divisor
+
+Divide(10, 2)
+
+sum = Divide(10, 2)
+
+print(sum)
